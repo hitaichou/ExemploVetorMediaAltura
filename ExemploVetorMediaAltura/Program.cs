5d3a9daa191123226888ff1f8f024a9251b8a773
hitaichou <holyfernandes@yahoo.co.uk>
2020-01-11 13:34:02 -0300

    Exemplo calculo de média usando vetor

--- ExemploVetorMediaAltura/Program.cs
+++ ExemploVetorMediaAltura/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ExemploVetorMediaAltura
 {
@@ -6,7 +7,29 @@ namespace ExemploVetorMediaAltura
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Criação de variável na memória stack
+            int n = int.Parse(Console.ReadLine());
+
+            //criação da variável do tipo vetor
+            //instanciação da variável de entrada
+            //esta ação cria na memória HEAP o tamanho do vetor.
+            double[] vet = new double[n]; 
+
+            //Para conseguir trabalhar com o vetor, é necessário ter um cursor
+            //para percorrer o cursor
+            for(int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double sum = 0;
+
+            for(int i = 0; i< n; i++)
+            {
+                sum = sum + vet[i];
+            }
+            double avg = sum / n;
+            Console.WriteLine("AVARAGE HEIGHT = " + avg.ToString("f2"), CultureInfo.InvariantCulture);
         }
     }
 }
